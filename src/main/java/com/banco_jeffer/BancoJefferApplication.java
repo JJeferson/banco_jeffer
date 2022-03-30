57c68d25be2100ff57670c30d7e0abc18b319d00
JJeferson <48735241+JJeferson@users.noreply.github.com>
2022-03-30 20:04:09 -0300

    Commit 0

---
+++ src/main/java/com/banco_jeffer/BancoJefferApplication.java
@@ -0,0 +1,13 @@
+package com.banco_jeffer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoJefferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancoJefferApplication.class, args);
+	}
+
+}
